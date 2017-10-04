dfba3df07368fab110fef53479d86501662b2bcf
Magne Rasmussen <magne.rasmussen@gmail.com>
2017-10-04 20:13:11 +0200

    Changed the ssh module to work on Cygwin.

--- modules/ssh/init.zsh
+++ modules/ssh/init.zsh
@@ -25,9 +25,16 @@ if [[ ! -S "$SSH_AUTH_SOCK" ]]; then
   source "$_ssh_agent_env" 2> /dev/null
 
   # Start ssh-agent if not started.
-  if ! ps -U "$LOGNAME" -o pid,ucomm | grep -q -- "${SSH_AGENT_PID:--1} ssh-agent"; then
-    mkdir -p "$_ssh_agent_env:h"
-    eval "$(ssh-agent | sed '/^echo /d' | tee "$_ssh_agent_env")"
+  if [[ $(uname -s) =~ '^CYGWIN' ]]; then
+    if ! ps -u "$LOGNAME" | grep -q -- "${SSH_AGENT_PID:--1} ssh-agent"; then
+      mkdir -p "$_ssh_agent_env:h"
+      eval "$(ssh-agent | sed '/^echo /d' | tee "$_ssh_agent_env")"
+    fi
+  else
+    if ! ps -U "$LOGNAME" -o pid,ucomm | grep -q -- "${SSH_AGENT_PID:--1} ssh-agent"; then
+      mkdir -p "$_ssh_agent_env:h"
+      eval "$(ssh-agent | sed '/^echo /d' | tee "$_ssh_agent_env")"
+    fi
   fi
 fi
 
